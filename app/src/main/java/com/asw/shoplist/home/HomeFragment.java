d658df87ba8bc513a5b18803076ba892cd5b7b52
Zain Cheema <z.a.cheema7777@gmail.com>
2021-01-23 15:55:12 +0500

    created HomeFragment's functionality

--- app/src/main/java/com/asw/shoplist/home/HomeFragment.java
+++ app/src/main/java/com/asw/shoplist/home/HomeFragment.java
@@ -2,22 +2,53 @@ package com.asw.shoplist.home;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.asw.shoplist.Event;
 import com.asw.shoplist.R;
 import com.asw.shoplist.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
     private FragmentHomeBinding viewDataBinding;
+    private HomeViewModel viewModel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         viewDataBinding = FragmentHomeBinding.inflate(inflater, container, false);
+        viewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory())
+                .get(HomeViewModel.class);
+        viewDataBinding.setViewmodel(viewModel);
         return viewDataBinding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        viewDataBinding.setLifecycleOwner(getViewLifecycleOwner());
+        setUpNavigation();
+    }
+
+    private void setUpNavigation() {
+        NavController navController = NavHostFragment.findNavController(this);
+        viewModel.observeAddListEvent().observe(getViewLifecycleOwner(), new Event.EventObserver<>(
+                add -> {
+                    if (add) navController.navigate(R.id.action_homeFragment_to_createListFragment);
+                }
+        ));
+        viewModel.observeCompareListsEvent().observe(getViewLifecycleOwner(), new Event.EventObserver<>(
+                compare -> {
+                    if (compare)
+                        navController.navigate(R.id.action_homeFragment_to_compareListsFragment);
+                }
+        ));
     }
 }
--- app/src/main/java/com/asw/shoplist/home/HomeViewModel.java
+++ app/src/main/java/com/asw/shoplist/home/HomeViewModel.java
@@ -1,6 +1,33 @@
 package com.asw.shoplist.home;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.asw.shoplist.Event;
+
 public class HomeViewModel extends ViewModel {
+    private MutableLiveData<Event<Boolean>> addListEvent;
+    private MutableLiveData<Event<Boolean>> compareListsEvent;
+
+    public HomeViewModel() {
+        addListEvent = new MutableLiveData<>();
+        compareListsEvent = new MutableLiveData<>();
+    }
+
+    public void addList() {
+        addListEvent.setValue(new Event(true));
+    }
+
+    public void compareLists() {
+        compareListsEvent.setValue(new Event(true));
+    }
+
+    public LiveData<Event<Boolean>> observeAddListEvent() {
+        return addListEvent;
+    }
+
+    public LiveData<Event<Boolean>> observeCompareListsEvent() {
+        return compareListsEvent;
+    }
 }
